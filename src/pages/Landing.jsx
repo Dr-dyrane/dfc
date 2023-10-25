f18ff0f34f987a57c8b6e462a3c068e72af70174
Dr-dyrane <halodyrane@gmail.com>
2023-10-25 23:09:53 +0100

    new file:   public/land.jpg

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -6,7 +6,7 @@ import { FcGoogle } from "react-icons/fc";
 class Landing extends Component {
 	render() {
 		return (
-			<div className="bg-purple-600  font-bold h-screen flex flex-col justify-center items-center p-4">
+			<div className="bg-landing bg-cover font-bold h-screen flex flex-col justify-center items-center p-4">
 				<img
 					src={dfc_logo}
 					alt="Dyrane's Farm Cast Logo"
@@ -19,10 +19,9 @@ class Landing extends Component {
 					</p>
 					<p className="text-lg text-slate-200">and Green Guidance</p>
 				</div>
-				<div className="h-1/3"></div>
+				<div className="h-1/2"></div>
 				<button
-					onClick={() => context.handleGoogleLogin()}
-					className="w-96 my-3 flex justify-center bg-slate-200 text-slate-700 font-bold py-3 rounded-2xl hover:bg-blue-700 hover:text-white focus:outline-none focus:ring focus:ring-blue-500"
+					className="w-96 my-3 flex justify-center bg-slate-200 text-slate-700 font-bold py-3 rounded-2xl hover:bg-green-600 hover:text-white focus:outline-none focus:ring focus:ring-green-600"
 				>
 					<span className="mr-2">
 						<FcGoogle size={24} />
@@ -31,7 +30,7 @@ class Landing extends Component {
 					{/* Include the FcGoogle icon */}
 				</button>
 				<Link to="/signup">
-					<button className="border bg-purple-600 w-96 text-white py-3 rounded-2xl hover:bg-purple-500">
+					<button className="border w-96 text-white py-3 rounded-2xl hover:bg-slate-200 hover:text-slate-600">
 						Sign up for free
 					</button>
 				</Link>
