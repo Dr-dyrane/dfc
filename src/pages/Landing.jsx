2d546d5d23457657519363dffebd24b6a66972ce
Dr-dyrane <halodyrane@gmail.com>
2023-10-25 22:36:19 +0100

    landing page take off

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import dfc_logo from "/dfc.png"; // Import your logo file
 import { Link } from "react-router-dom"; // Assuming you're using React Router for navigation
+import { FcGoogle } from "react-icons/fc";
 
 class Landing extends Component {
 	render() {
@@ -18,15 +19,25 @@ class Landing extends Component {
 					</p>
 					<p className="text-lg text-slate-200">and Green Guidance</p>
 				</div>
-        <div className="h-1/3"></div>
+				<div className="h-1/3"></div>
+				<button
+					onClick={() => context.handleGoogleLogin()}
+					className="w-96 my-3 flex justify-center bg-slate-300 text-slate-700 font-bold py-3 rounded-2xl hover:bg-blue-700 hover:text-white focus:outline-none focus:ring focus:ring-blue-500"
+				>
+					<span className="mr-2">
+						<FcGoogle size={24} />
+					</span>
+					Log in with Google
+					{/* Include the FcGoogle icon */}
+				</button>
 				<Link to="/signup">
-					<button className="bg-green-500 text-white py-2 px-4 rounded-lg hover:bg-green-700">
-						Get Started
+					<button className="border bg-purple-600 w-96 text-white py-3 px-4 rounded-2xl hover:bg-purple-500">
+						Sign up for free
 					</button>
 				</Link>
 				<p className="text-white my-4">
 					Already have an account?{" "}
-					<Link to="/login" className="text-green-500">
+					<Link to="/login" className="">
 						Log In
 					</Link>
 				</p>
