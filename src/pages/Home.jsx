cc85806f47d01080008507e76b5c6000287d2a37
Dr-dyrane <halodyrane@gmail.com>
2023-10-29 12:55:56 +0100

    modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,15 +26,20 @@ class Home extends Component {
 		const { dfc } = this.state;
 
 		return (
-			<div className="flex flex-col items-center justify-center font-semibold p-4 h-screen bg-slate-200">
+			<div className="flex flex-col items-center justify-center font-semibold p-10 min-h-screen bg-gradient-to-br from-slate-200 to-slate-100">
 				<div>
 					<UserProfile />
 				</div>
-				<Card className="rounded-xl">
+				<Card className="rounded-3xl bg-gradient-to-br text-slate-100 from-black to-slate-700 max-w-3xl">
 					<Text className="text-6xl font-bold text-center mb-10">
 						Dyrane's Farm Cast
 					</Text>
-					<Subtitle className="text-center">Powered by OpenAI, Next.js, Tailwind SS, Tremor</Subtitle>
+					<Subtitle className="text-xl text-center">
+						Powered by OpenAI, Next.js, Tailwind CSS, Tremor
+					</Subtitle>
+					<Divider className="my-10"></Divider>
+					<Card className="bg-gradient-to-br from-slate-200 to-slate-100 rounded-2xl">
+					</Card>
 				</Card>
 			</div>
 		);
