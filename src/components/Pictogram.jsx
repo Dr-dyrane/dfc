09b721c29d8d5f63f41e2a230be46e20e267d808
Dr-dyrane <halodyrane@gmail.com>
2023-11-01 18:53:57 +0100

    modified:   src/components/Pictogram.jsx

--- src/components/Pictogram.jsx
+++ src/components/Pictogram.jsx
@@ -3,6 +3,7 @@ import {
 	RiTempHotFill,
 	RiWaterFlashFill,
 	RiUmbrellaFill,
+	RiWindyFill,
 } from "react-icons/ri";
 
 const WeatherPictogram = ({ weatherData }) => {
@@ -12,7 +13,7 @@ const WeatherPictogram = ({ weatherData }) => {
 
 	const temperature = weatherData.current.temperature_2m;
 	const humidity = weatherData.current.relativehumidity_2m;
-	const precipitation = weatherData.current.precipitation;
+	const windSpeed  = weatherData.current.windspeed_10m;
 
 	return (
 		<div className="p-4 m-4 rounded-2xl font-bold shadow-lg bg-gradient-to-br from-blue-600 to-purple-600">
@@ -33,11 +34,11 @@ const WeatherPictogram = ({ weatherData }) => {
 			</div>
 
 			<div className="flex items-center justify-between space-x-4 p-4">
-				<div className="text-2xl text-green-400">
-					<RiUmbrellaFill />
+				<div className="text-2xl text-purple-400">
+					<RiWindyFill  />
 				</div>
 				<p className="text-xl text-blue-900 font-semibold"></p>
-				<p className="text-lg">{precipitation} mm</p>
+				<p className="text-lg">{windSpeed} km/h</p>
 			</div>
 		</div>
 	);
