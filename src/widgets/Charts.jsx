909cead7226c43700d618ec58dec6adeba73223d
Dr-dyrane <halodyrane@gmail.com>
2023-11-11 21:07:20 +0100

    new file:   src/widgets/charts/RainChart.jsx

--- src/widgets/Charts.jsx
+++ src/widgets/Charts.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import TempChart from './charts/TempChart'
+import RainChart from './charts/RainChart'
 
 function Charts({weatherData}) {
   return (
-    <div className='space-y-3 p-4'><TempChart weatherData={weatherData}/></div>
+    <div className='space-y-3 p-4'>
+        <TempChart weatherData={weatherData}/>
+        <RainChart weatherData={weatherData}/>
+        </div>
   )
 }
 
---
+++ src/widgets/charts/RainChart.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Card, AreaChart, Title, NumberInput } from "@tremor/react";
+
+function RainChart({ weatherData }) {
+    if (
+        !weatherData ||
+        !weatherData.hourly ||
+        !Array.isArray(weatherData.hourly.precipitation_probability)
+      ) {
+        return null;
+      }
+	const hourly = weatherData?.hourly.time.map((time) =>
+		new Date(time)
+			.toLocaleTimeString("en-US", {
+				hour: "numeric",
+				hour12: false,
+			})
+	).slice(1, 25);
+
+	const data = hourly.map((hour, index) => ({
+		time: hour,
+		"Rain (%)": weatherData.hourly.precipitation_probability[index],
+	}));
+
+	const dataFormatter = (number) => `${number} %`;
+
+	return (
+		<Card className="rounded-2xl sm:w-[25rem] lg:w-[45rem] xl:w-[81rem]">
+			<Title>Chances of Rain</Title>
+			<AreaChart
+				className="mt-3"
+				data={data}
+				showLegend
+				index="time"
+				categories={["Rain (%)"]}
+				colors={["blue"]}
+				minValue={0}
+				maxValue={100}
+				valueFormatter={dataFormatter}
+                yAxisWidth={45}
+			/>
+		</Card>
+	);
+}
+
+export default RainChart;
